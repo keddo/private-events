22bf86c1c4478fe411d0c70a74d1dc98371cad0a
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-28 17:15:21 +0300

    Add show, and index pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
+    before_action :set_user, except: %i[new index create ]
+    def index
+    @users = User.all
+    end
+    def new 
+    @user = User.new
+    end
 
- def new 
-   @user = User.new
+    def create
+    @user = User.new(user_params);
+    if @user.save
+        flash[:success] = "User registered successfully"
+        redirect_to user_path(@user)
+    else
+        render :new
+    end
+    end
+    def show 
+
+    end
+ private 
+
+ def set_user
+  @user = User.find(params[:id])
  end
- 
+ def user_params
+  params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+ end
 end
