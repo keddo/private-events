e9f27156a267e71f8b5780015f75ba92f3639a1f
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-30 12:14:57 +0300

    Added events list in users show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,26 @@
 class EventsController < ApplicationController
-    
+    before_action :set_event, except: %i[index new create]
+    def index
+    end
+    def new
+    end
+    def create
+    end
+    def show
+    end
+    def edit
+    end
+    def update
+    end
+    def destory
+    end
+
+    private
+    def event_params
+        params.require(:user).permit(:eventdate, :eventdescription, :eventlocation, :eventname, :creator_id)
+    end
+
+    def set_event
+        @event = Evnet.find(params[:id])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :invitations, foreign_key: :attendee_id
-    has_many :attended_events, through: :invitations, source: attended_event
+    has_many :attended_events, through: :invitations
     has_many :events, foreign_key: :creator_id
 
     has_secure_password
