d80da517822c17bd93e729a6c802eb6cfab3d5d5
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-30 15:28:28 +0300

    devise fully configured

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,12 @@
 class UsersController < ApplicationController
-    before_action :set_user, except: %i[new index create ]
-    def index
-    @users = User.all
-    end
-    def new 
-    @user = User.new
-    end
+    private 
 
-    def create
-    @user = User.new(user_params);
-    if @user.save
-        flash[:success] = "User registered successfully"
-        redirect_to user_path(@user)
-    else
-        render :new
-    end
-    end
-    def show 
+   
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
 
-    end
- private 
-
- def set_user
-  @user = User.find(params[:id])
- end
- def user_params
-  params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
- end
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { users: 'users' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
   post 'login', to: 'sessions#create'
   get 'login', to: 'sessions#new' 
   delete 'login', to: 'sessions#destroy'
