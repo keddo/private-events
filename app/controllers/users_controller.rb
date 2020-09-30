0f6d9c150d27c004d79f0dd3941d86dc5772bb92
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-09-30 15:44:26 +0200

    Add logout and navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    private 
+    private
 
-   
+
   def sign_up_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
