45cd7de4c559b1dabda1a859f237aea5ca5aa694
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-30 11:45:24 +0300

    Add event lists to creator show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :invitations, foreign_key: :attendee_id
     has_many :attended_events, through: :invitations, source: attended_event
     has_many :events, foreign_key: :creator_id
+
+    has_secure_password
 end
