c84fca2fe2d522c0053fb0175815c322282da1ee
keddo <adu.kedir.abdurahman@gmail.com>
2020-10-05 17:07:17 +0300

    Add users list

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'login', to: 'sessions#create'
-  get 'login', to: 'sessions#new' 
-  delete 'login', to: 'sessions#destroy'
-
+  resources :users, only: [:index]
   resources :events
   root 'events#index'
 end
