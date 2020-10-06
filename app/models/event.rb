7b5ed056faa6103371979f676a32cf819267272f
keddo <adu.kedir.abdurahman@gmail.com>
2020-10-06 11:39:01 +0300

    Add user, event validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
+    validates :eventdate, presence: true
+    validates :eventdescription, presence: true, length: { in: 20..200 }
+    validates :eventname, presence: true
+    validates :eventlocation, presence: true
+    validates :creator_id, presence: true
+
+    
     has_many :invitations, foreign_key: :attended_event_id
     has_many :attendees, through: :invitations, source: :attendee
     belongs_to :creator, class_name: "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :name, length: { in: 5..25 }
+    validates :username, length: { in: 5..15 }
+    
     has_many :invitations, foreign_key: :attendee_id
     has_many :attended_events, through: :invitations
     has_many :events, foreign_key: :creator_id
