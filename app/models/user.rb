61bcea0928276ed2fe7e2df98b7f20f772d3aa9a
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-10-06 15:54:30 +0200

    Add rspec and user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 
     validates :name, length: { in: 5..25 }
     validates :username, length: { in: 5..15 }
+    validates :email,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
+              message: "Email invalid"  },
+              uniqueness: { case_sensitive: false },
+              length: { minimum: 4, maximum: 254 }
 
     has_many :invitations, foreign_key: :attendee_id
     has_many :attended_events, through: :invitations
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'capybara/rspec'
+require 'spec_helper'
+
+RSpec.describe User, type: :model do
+  context 'validation tests' do
+    it 'Should be valid if the attributes are' do
+      expect(User.new(name: 'ThisIsName', email: 'blablabla@gmail.com')).to be_valid
+    end
+
+    it 'Shouldn´t save if the name is less than 5 or more than 25 characters' do
+      user = User.new(name: 'Helo', username: 'Test', email: 'helloworld@gmail.com', password: '123456', password_confirmation: '123456')
+      expect(user.save).to eq(false)
+    end
+
+    it 'Shouldn´t save if the username is less than 5 or more than 15 characters' do
+      user = User.new(name: 'Helo', username: 'Test', email: 'helloworld@gmail.com', password: '123456', password_confirmation: '123456')
+      expect(user.save).to eq(false)
+    end
+
+    it 'Shouldn´t save if the email if is invalid'
+      user = User.new(name: 'Helo', username: 'Test', email: 'helloworldgmail', password: '123456', password_confirmation: '123456')
+      expect(user.save).to eq(false)
+    end
+
+    it 'Password should contain more than 6 characters'
+      user = User.new(name: 'Helo', username: 'Test', email: 'helloworldgmail', password: '1234', password_confirmation: '1234')
+      expect(user.save).to eq(false)
+    end
+  end
+end
